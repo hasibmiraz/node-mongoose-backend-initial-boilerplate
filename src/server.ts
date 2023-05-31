5c8426884350c4335fac2f952e488ff1d06f02da
hasibmiraz <hasibmiraz09@gmail.com>
2023-05-31 23:43:42 +0600

    [Done] Setup husky for precommit Issues

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from 'mongoose';
-import config from './config';
-import { app } from './app';
+import mongoose from 'mongoose'
+import config from './config'
+import { app } from './app'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
 
     app.listen(config.port, () =>
       console.log(`App listening to port ${config.port}`)
-    );
-    console.log(`DB connected successfully`);
+    )
+    console.log(`DB connected successfully`)
   } catch (err: unknown) {
-    console.log(`Failed to connect to db: ${err}`);
+    console.log(`Failed to connect to db: ${err}`)
   }
 }
 
-main();
+main()
